db598776464f5a149823e9300e220b0f64bf080b
Charlie Depman <cdepman@gmail.com>
2022-10-02 20:50:57 -0400

    update manifesto copy

--- src/components/ManifestoText.js
+++ src/components/ManifestoText.js
@@ -9,25 +9,28 @@ export const ManifestoText = () => (
       <Text as="i">Decommodification</Text> is a core principle of Burning Man.
       In 2022, Burning Man moved closer to full decommodification by removing
       coffee purchasing from Center Camp. Coffee is provided as a gift by other
-      camps. Ice has yet to be decommodified and can be purchased on playa at an
-      establishment called Arctica by cash, credit card or Apple pay, but it
-      really kills the decommodification vibe, has been getting more expensive,
-      and can be a burden for camps to get every day.
+      camps. Ice has yet to be decommodified - camps purchase it on playa at an
+      establishment called Arctica by cash, credit card or Apple pay. This kills
+      the decommodification vibe and can be a burden for camps to retrieve every
+      day - not to mention the price has been going up. We are proposing to
+      decommodify ice by providing it for free.
     </Text>
     <br></br>
     <Text>
       Another principle of Burning Man is <Text as="i">Leaving No Trace</Text>.
       The ice that Arctica serves has a large environmental impact because it
       comes in disposable plastic bags and it must be trucked into the desert on
-      huge, gas-powered refrigerated trucks. Not great.
+      huge, gas-powered refrigerated trucks. We are proposing to manufacture ice
+      on playa with solar power and deliver it in insulated reusable containers.
     </Text>
     <br></br>
     <Text>
-      A third principle of Burning Man is <Text as="i">Gifting</Text>. We are
-      proposing to create a solar-powered ice camp that gifts ice in reusable
-      containers. We want to start small, creating enough ice to provide to
-      ourselves and immediate neighbors. In the future, we can potentially scale
-      up and provide more solar ice to the playa.
+      A third principle of Burning Man is <Text as="i">Gifting</Text>. One of
+      Ice Ice Maybe's major gifts at Burning Man will be ice. We want to start
+      small, creating enough ice to provide to ourselves and surrounding
+      neighbors. We can scale up and provide more solar ice to the playa in the
+      future. One vision is to have many automated solar ice vending stations
+      positioned around the residential areas of Burning Man.
     </Text>
   </Box>
 );
