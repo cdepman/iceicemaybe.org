7bcb23ad254f1ecb6722af770e4b8639d05bfc35
Charlie Depman <cdepman@gmail.com>
2023-05-05 01:06:32 -0400

    update link color to fit theme

--- src/components/LinkToOurVision.js
+++ src/components/LinkToOurVision.js
@@ -2,6 +2,7 @@ import { Link } from '@chakra-ui/react';
 export const LinkToOurVision = () => (
   <Link
     textDecoration="underline"
+    color="#00a0dc"
     href="https://www.wordsfromthefuture.org/p/visiting-ice-ice-maybe"
   >
     Read our long-term vision here!
